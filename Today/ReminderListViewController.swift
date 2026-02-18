866f0bc37ea5fef27b379a87f41912a349dfbe80
tomchoi <qjatn0545@naver.com>
2026-02-18 21:57:03 +0900

    feat: DiffableDataSource와 Cell 등록

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -8,14 +8,32 @@
 import UIKit
 
 final class ReminderListViewController: UICollectionViewController {
-    
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+
+    var dataSource: DataSource?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
+
+        let cellRegistration = UICollectionView.CellRegistration {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+
+        dataSource = DataSource(
+            collectionView: collectionView,
+            cellProvider: {
+                $0.dequeueConfiguredReusableCell(using: cellRegistration, for: $1, item: $2)
+            }
+        )
     }
-    
+
     private func listLayout() -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
         listConfiguration.showsSeparators = false
