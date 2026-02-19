993f7141bffb05946999729fbb72d9e9f4034955
tomchoi <qjatn0545@naver.com>
2026-02-19 23:34:02 +0900

    feat: TextFieldContentView를 UIContentView 및 UIContentConfiguration 패턴으로 리팩토링하고 관련 설정 메서드를 추가합니다.

--- Today/ContentViews/TextFieldContentView.swift
+++ Today/ContentViews/TextFieldContentView.swift
@@ -7,14 +7,29 @@
 
 import UIKit
 
-final class TextFieldContentView: UIView {
+final class TextFieldContentView: UIView, UIContentView {
+    struct Configuration: UIContentConfiguration {
+        var text: String? = ""
+
+        func makeContentView() -> UIView & UIContentView {
+            return TextFieldContentView(self)
+        }
+    }
+
+    var configuration: UIContentConfiguration {
+        didSet {
+            configure(configuration: configuration)
+        }
+    }
+
     let textField = UITextField()
 
     override var intrinsicContentSize: CGSize {
         CGSize(width: 0, height: 44)
     }
 
-    init() {
+    init(_ configuration: UIContentConfiguration) {
+        self.configuration = configuration
         super.init(frame: .zero)
         addPinnedSubview(textField, insets: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
         textField.clearButtonMode = .whileEditing
@@ -22,5 +37,16 @@ final class TextFieldContentView: UIView {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? Configuration else { return }
+        textField.text = configuration.text
+    }
+}
+
+extension UICollectionViewListCell {
+    func textFieldConfiguration() -> TextFieldContentView.Configuration {
+        TextFieldContentView.Configuration()
     }
 }
