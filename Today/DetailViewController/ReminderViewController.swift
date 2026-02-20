b5dcf66a12bec4694312f365e14fe1ba89b42059
tomchoi <qjatn0545@naver.com>
2026-02-20 09:51:29 +0900

    feat: 편집 모드에서 변경 사항을 취소하고 보기 모드로 돌아갈 수 있는 취소 버튼을 추가했습니다.

--- Today/DetailViewController/ReminderViewController.swift
+++ Today/DetailViewController/ReminderViewController.swift
@@ -89,7 +89,17 @@ final class ReminderViewController: UICollectionViewController {
         cell.tintColor = .todayPrimaryTint
     }
 
+    @objc func didCancelEdit() {
+        workingReminder = reminder
+        setEditing(false, animated: true)
+    }
+
     private func prepareForEditing() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .cancel,
+            target: self,
+            action: #selector(didCancelEdit)
+        )
         updateSnapshotForEditing()
     }
 
@@ -114,6 +124,7 @@ final class ReminderViewController: UICollectionViewController {
     }
 
     private func prepareForViewing() {
+        navigationItem.leftBarButtonItem = nil
         if workingReminder != reminder {
             reminder = workingReminder
         }
