f70903d8d42eba80728e9448ea045d291151de35
tomchoi <qjatn0545@naver.com>
2026-02-20 10:55:42 +0900

    feat: 내비게이션 바에 미리 알림 추가 버튼을 구현하고 스타일을 설정했습니다.

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -29,6 +29,18 @@ final class ReminderListViewController: UICollectionViewController {
             )
         }
 
+        let addButton = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(didPressAddButton(_:))
+        )
+        addButton.accessibilityLabel = NSLocalizedString(
+            "Add reminder",
+            comment: "Add button accessibility label"
+        )
+        navigationItem.rightBarButtonItem = addButton
+        navigationItem.style = .navigator
+
         var snapshot = SnapShot()
         snapshot.appendSections([0])
         snapshot.appendItems(Reminder.sampleData.map(\.id))
