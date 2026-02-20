ada58ba24e71eadaf69f47dfb56f4800f5037b5c
tomchoi <qjatn0545@naver.com>
2026-02-20 18:00:11 +0900

    feat: 오류 표시 함수를 추가했습니다.

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -136,6 +136,25 @@ final class ReminderListViewController: UICollectionViewController {
         navigationController?.pushViewController(viewController, animated: true)
     }
 
+    func showError(_ error: Error) {
+        let alertTitle = NSLocalizedString("Error", comment: "Error alert title")
+        let alert = UIAlertController(
+            title: alertTitle,
+            message: error.localizedDescription,
+            preferredStyle: .alert
+        )
+        let actionTitle = NSLocalizedString("OK", comment: "Alert OK button title")
+        alert.addAction(
+            UIAlertAction(
+                title: actionTitle,
+                style: .default,
+                handler: { [weak self] _ in
+                    self?.dismiss(animated: true)
+                }
+            )
+        )
+    }
+
     private func listLayout() -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
         listConfiguration.headerMode = .supplementary
